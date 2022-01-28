1a225ce4511043ba825aadf888fd9c5b66a771c8
suraj-techindustan <suraj@techindustan.com>
2022-01-28 17:50:03 +0530

    improve routes

--- src/Book.js
+++ src/Book.js
@@ -1,7 +1,7 @@
 import http from "./Http-comman"
 class Books{
     getAll(){
-        return http.get("/allBooks");
+        return http.get("/api/v1/book/allBooks");
     }
 }
 export default new Books();
